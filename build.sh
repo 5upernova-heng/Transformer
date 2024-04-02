9a683fc930be02667c1b2c65d95bc4e56d322b66
Heng Yu <h3ngyu@gmail.com>
2024-04-02 21:12:21 +0800

    Upd: gitignore

--- build.sh
+++
@@ -1,5 +0,0 @@
-rm -rf build
-mkdir -p build
-cd build
-cmake -DCMAKE_PREFIX_PATH=/home/hengyu/libtorch -DCMAKE_PREFIX_PATH=/opt/OpenBLAS-0.3.26 -DCMAKE_C_COMPILER=/usr/local/bin/gcc -DCMAKE_CXX_COMPILER=/usr/local/bin/g++ .. 
-cmake --build . --config Debug --target all -j 18 --
