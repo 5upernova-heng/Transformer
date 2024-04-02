a1c953d7fd81320058abdca8e08d3b7d6921fbb6
Heng Yu <h3ngyu@gmail.com>
2024-04-02 21:06:49 +0800

    Upd: use one para matrix instead of an array of matrices

--- src/module/attention.cpp
+++ src/module/attention.cpp
@@ -3,14 +3,14 @@
 Attention::Attention(int n, int m, int d_model, int dk, int dv, int h)
         : n(n), m(m), d_model(d_model), dk(dk), dv(dv), h(h) {
     for (int i = 0; i < h; i++) {
-        Wq.push_back(std::make_shared<Mat2D>(d_model, dk));
-        Wk.push_back(std::make_shared<Mat2D>(d_model, dk));
-        Wv.push_back(std::make_shared<Mat2D>(d_model, dv));
         Q.push_back(std::make_shared<Mat2D>(n, dk));
         K.push_back(std::make_shared<Mat2D>(m, dk));
         V.push_back(std::make_shared<Mat2D>(m, dv));
         outputs.push_back(std::make_shared<Mat2D>(n, dv));
     }
+    Wq = std::make_shared<Mat2D>(d_model, d_model);
+    Wk = std::make_shared<Mat2D>(d_model, d_model);
+    Wv = std::make_shared<Mat2D>(d_model, d_model);
     Wo = std::make_shared<Mat2D>(h * dv, d_model);
 }
 
@@ -18,17 +18,14 @@ Attention::Attention(int n, int m, int d_model, int dk, int dv, int h)
 /*
  * (n, d_model) -> (n, d_v)
  */
-void Attention::SingleHeadAttention(Mat2D &q, Mat2D &k, Mat2D &v, Mat2D &out, int index, bool mask) {
+void Attention::SingleHeadAttention(Mat2D &out, int index, bool mask) {
     if (index < 0 or index >= Q.size()) {
         printf("Channel number h out of range\n");
     }
-    Mat2D::multiply(q, *Wq[index], *Q[index]);
-    Mat2D::multiply(k, *Wk[index], *K[index]);
-    Mat2D::multiply(v, *Wv[index], *V[index]);
     Mat2D attention_matrix(n, m), K_T(dk, m);
     Mat2D::transpose(*K[index], K_T);
     Mat2D::multiply(*Q[index], K_T, attention_matrix);
-    attention_matrix /= sqrt(dk);
+    attention_matrix /= (float) sqrt(dk);
     if (mask)
         attention_matrix.mask();
     attention_matrix.softmax();
@@ -39,9 +36,28 @@ void Attention::MultiheadAttention(Mat2D &input, Mat2D &output, bool mask) {
     Attention::MultiheadAttention(input, input, input, output, mask);
 }
 
-void Attention::MultiheadAttention(Mat2D &q, Mat2D &k, Mat2D &v, Mat2D &output, bool mask) {
+void Attention::split(Mat2D &mat, const std::vector<std::shared_ptr<Mat2D>>& dests) {
     for (int i = 0; i < h; i++) {
-        SingleHeadAttention(q, k, v, *outputs[i], i, mask);
+        for (int j = 0; j < mat.sizes.first; j++) {
+            for (int k = 0; k < mat.sizes.second / h; k++) {
+                dests[i]->data[j][k] = mat.data[j][k + i * h];
+            }
+        }
+    }
+}
+
+void Attention::MultiheadAttention(Mat2D &q, Mat2D &k, Mat2D &v, Mat2D &output, bool mask) {
+    Mat2D temp_Q(q.sizes.first, d_model);
+    Mat2D temp_K(k.sizes.first, d_model);
+    Mat2D temp_V(v.sizes.first, d_model);
+    Mat2D::multiply(q, *Wq, temp_Q);
+    Mat2D::multiply(k, *Wk, temp_K);
+    Mat2D::multiply(v, *Wv, temp_V);
+    Attention::split(temp_Q, Q);
+    Attention::split(temp_K, K);
+    Attention::split(temp_V, V);
+    for (int i = 0; i < h; i++) {
+        SingleHeadAttention(*outputs[i], i, mask);
     }
     Mat2D concatenated(n, dv * h);
     Mat2D::concat(outputs, concatenated);
--- src/module/attention.h
+++ src/module/attention.h
@@ -6,12 +6,12 @@
 class Attention {
 public:
     int n, m, d_model, dk, dv, h;
-    std::vector<std::shared_ptr<Mat2D>> Wq, Wk, Wv, Q, K, V, outputs;
-    std::shared_ptr<Mat2D> Wo;
+    std::vector<std::shared_ptr<Mat2D>> Q, K, V, outputs;
+    std::shared_ptr<Mat2D> Wo, Wq, Wk, Wv;
 
     Attention(int n, int m, int d_model, int dk, int dv, int h);
 
-    void SingleHeadAttention(Mat2D &q, Mat2D &k, Mat2D &v, Mat2D &out, int index, bool mask);
+    void SingleHeadAttention(Mat2D &out, int index, bool mask);
 
     void MultiheadAttention(Mat2D &input, Mat2D &output, bool mask);
 
@@ -19,6 +19,8 @@ public:
 
     ~Attention();
 
+private:
+    void split(Mat2D &mat, const std::vector<std::shared_ptr<Mat2D>>& dests);
 };
 
 #endif //TRANSFORMER_ATTENTION_H
